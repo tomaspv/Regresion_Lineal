f1aa06f9818e8796ec782a550ad940435e0beb8d
tomaspv <32546168+tomaspv@users.noreply.github.com>
2018-05-03 11:29:15 -0300

    Segundo commit
    Upload via web

--- SRC/Regresion_Lineal.py
+++ SRC/Regresion_Lineal.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Modelo de regresion lineal en Python
+
+@author: TPV
+"""
+
+
 # Importamos librerias con las que vamos a trabajar
 
-#Para 
+#Para trabajar con matrices y realizar operaciones matematicas
 import numpy as np
 #Para hacer los graficos
 import matplotlib.pyplot as plt
@@ -8,13 +16,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #Importamos el data set
-ds = pd.read_csv('../DATASET/student/student-mat.csv',sep =';')
 ds = pd.read_excel('../DATASET/Wine.xls')
 
 #Separamos las variables para trabajarlas
 x = ds.loc[:, ['Alcohol']].values
 y = ds.loc[:, ['Color intensity']].values
 
+#Observamos si a priori existe una correlacion entre las variables
 plt.scatter(x,y)
 
 #Dividimos el data set en dos partes. Entrenamiento y prueba.
@@ -29,23 +37,42 @@ regresor.fit(x_train, y_train)
 #Ejecutamos el modelo
 y_pred = regresor.predict(x_test)
 
-#Evaluamos el modelo#
-#####################
-#R^2 (Multiple R-Squared) es un indicador de bondad de ajuste de mi modelo.R^2 oscila entre 0 y 1. Mientras
-#mas se acerque a 1 , indica que el modelo se ajusta bien a los datos.
-regresor.score(x_test,y_pred)
-
-metrics
-
 #intercept - ordenada al origen
 regresor.intercept_
 
 #coef - Por cada unidad de alcohol que aumenta, se incrementa en 1,69 el coeficiente de color del vino
 regresor.coef_
 
+
+#####################
+#Evaluamos el modelo#
+#####################
+
+#Utilizamos la libreria statsmodels para hallar el valor de Tstudent y Pvalue
+import statsmodels.api as sm
+
+#Analizamos Tstudent y Pvalue
+#Si el valor de Tstudent es cero, diremos que no hay relacion entre las variables X e Y.
+#Si el valor de P-Value es muy bajo se suelen tomar valores menores a 0,05 para considerar que es bajo, decimos que hay relacion entre las variables X e Y
+
+est = sm.OLS(y, x)
+est2 = est.fit()
+print(est2.summary())
+
+#Creamos una tabla para observar el valor real del numero y la prediccion que realizo el modelo.
+pred_ds = pd.DataFrame(y_pred, columns=['Prediccion'])
+pred_ds = pred_ds.assign(Valor_Real= y_test)
+pred_ds
+
+#Utilizamos la libreria sklearn para halar metricas
+from sklearn import metrics
+
+#EVS - Explanied_variance_score
+#Es una medida que me indica que tan dispersos son los datos que estoy analizando. Oscila entre 0 y 1. Mientas mas cerca este de 1 mejor.
+metrics.explained_variance_score(y_test,y_pred)
+
 #MAE - Mean Obsolute error
 #Es la media absoluta de los errores
-from sklearn import metrics
 metrics.mean_absolute_error(y_test, y_pred)
 
 #MSE - Mean Squared error
@@ -56,9 +83,16 @@ metrics.mean_squared_error(y_test, y_pred)
 #Es el MSE pero la raiz, es interpretable en la unidad de la variable dependiente "Y"
 np.sqrt(metrics.mean_squared_error(y_test, y_pred))
 
+#R^2 (Multiple R-Squared) es un indicador de bondad de ajuste de mi modelo.R^2 oscila entre 0 y 1. Mientras
+#mas se acerque a 1 , indica que el modelo se ajusta bien a los datos.
+metrics.r2_score(y_test,y_pred)
 
 
-# Visualising the Training set results
+#####################
+#Graficamos el modelo#
+#####################
+
+# Ploteamos el conjunto de entrenamiento
 plt.scatter(x_train, y_train, color = 'red')
 plt.plot(x_train, regresor.predict(x_train), color = 'blue')
 plt.title('Alcohol vs Intensidad del color (Training set)')
@@ -66,10 +100,15 @@ plt.xlabel('Alcohol')
 plt.ylabel('Intensidad del color')
 plt.show()
 
-# Visualising the Test set results
+# Ploteamos el conjunto de prueba
 plt.scatter(x_test, y_test, color = 'red')
 plt.plot(x_train, regresor.predict(x_train), color = 'blue')
 plt.title('Alcohol vs Intensidad del color (Test set)')
 plt.xlabel('Alcohol')
 plt.ylabel('Intensidad del color')
 plt.show()
+
+
+
+
+
